45b59cb1523ec50255bdd90b92e47b7dd6c699e9
e-e-e <e-e-e@users.noreply.github.com>
2017-03-28 21:52:28 +1100

    add dist to git for giturl import in dat-libary; npm does not run prepublish on git url installs

---
+++ dist/catalog.js
@@ -0,0 +1,325 @@
+'use strict';Object.defineProperty(exports, "__esModule", { value: true });exports.Catalog = undefined;exports.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+createCatalog = createCatalog;var _path = require('path');var _path2 = _interopRequireDefault(_path);var _fs = require('fs');var _fs2 = _interopRequireDefault(_fs);var _bluebird = require('bluebird');var _bluebird2 = _interopRequireDefault(_bluebird);var _knex = require('knex');var _knex2 = _interopRequireDefault(_knex);var _anotherNameParser = require('another-name-parser');var _anotherNameParser2 = _interopRequireDefault(_anotherNameParser);var _chalk = require('chalk');var _chalk2 = _interopRequireDefault(_chalk);var _config = require('./config');var _config2 = _interopRequireDefault(_config);var _dat = require('./dat');var _dat2 = _interopRequireDefault(_dat);var _opf = require('./opf');var _filesystem = require('./utils/filesystem');function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @todo: this.db.close(); should be called on shutdown
+// Class definition
+class Catalog {constructor(baseDir) {this.getDats = () => this.db('dats').select();this.getDat = key => this.db('dats').select().where('dat', key);this.baseDir = baseDir;this.dats = [];this.db = (0, _knex2.default)({ client: 'sqlite3', connection: { filename: _path2.default.format({ dir: this.baseDir, base: 'catalog.db' }) }, useNullAsDefault: true });this.isReady = false;}initDatabase() {return this.db.schema.createTableIfNotExists('dats', table => {table.string('dat');table.string('name');table.string('dir'); // table.unique('dat');
+    }).createTableIfNotExists('texts', table => {table.string('dat');table.string('title_hash');table.string('file_hash');table.string('author');table.string('author_sort');table.string('title');table.string('file');}).createTableIfNotExists('more_authors', table => {table.string('title_hash');table.string('author'); // table.unique('title_hash');
+    }).then(() => {this.isReady = true;}).catch(e => console.error(e));} // Every imported and added dat gets added to the `dats` table of the database. If
+  // the directories are deleted then these db entries are useless and should be removed.
+  // This will simply confirm that every dat directory in the db still exists.
+  cleanupDatsRegistry() {console.log('Cleaning up the dats registry');return this.getDats().map(dat => dat).filter(dat => (0, _filesystem.notADir)(dat.dir)).each(dat => {console.log(`Removing: ${_chalk2.default.bold(dat.dir)} (directory does not exist)`);return this.removeDatFromDb(dat.dat).then(() => this.clearDatEntries(dat.dat));}).then(() => this);} // Look inside the base directory for any directories that seem to be dats
+  discoverDats() {return (0, _filesystem.getDirectories)(this.baseDir).map(name => {console.log(`Attempting to load dir: ${_chalk2.default.bold(name)} as a dat`);const opts = { name, createIfMissing: false, sparse: true };return this.importDat(opts);}).then(() => this.cleanupDatsRegistry()).then(() => this.importDatsFromDB()).then(() => this);} // Imports dats listed in the dats table of the database
+  importDatsFromDB() {this.getDats().map(dat => dat).filter(dat => (0, _filesystem.notADir)(dat.dir)) // directory exists
+    .filter(dat => !dat.dir.startsWith(this.baseDir)) // not in data directory
+    .filter(dat => !(dat.key in this.dats.keys())) // not in registry
+    .each(dat => this.importDir(dat.dir, dat.name)).then(() => console.log('Imported dats from DB'));} // Imports a directory on the local filesystem as a dat.
+  // This should not be called on any directories inside `dataDir`, which are loaded differently
+  importDir(directory, name = false) {console.log(`Attempting to import local directory: ${directory}`);const opts = { directory, name: name || directory.split(_path2.default.sep).slice(-1)[0] };return this.importDat(opts);} // Importing a remote dat by its key
+  importRemoteDat(key, name = false) {console.log(`Attempting to import remote dat: ${key}`);const opts = { key, name: name || key, sparse: true };return this.importDat(opts);} // Does the work of importing a functional dat into the catalog
+  importDat(opts) {if ('key' in opts && opts.key in this.dats) {// The dat is already loaded, we shouldn't reimport it
+      console.log(`You are trying to import a dat that is already loaded: ${opts.key}`);return _bluebird2.default.resolve(false);}if (!opts.directory) {opts.directory = _path2.default.format({ dir: this.baseDir, base: opts.name ? opts.name : opts.key });}const newDat = new _dat2.default(opts, this);return newDat.run().then(() => this.registerDat(newDat)).then(() => (0, _dat.importFiles)(newDat)).then(() => (0, _dat.listDatContents)(newDat.dat)).each(entry => this.importDatEntry(newDat, entry)).catch(err => {console.log(`* Something went wrong when importing ${opts.directory}`);console.log(err);});} // Registers dat in catalog array and in database (@todo)
+  registerDat(dw) {const datkey = dw.dat.key.toString('hex');console.log(`Adding dat (${datkey}) to the catalog.`);return this.removeDatFromDb(datkey).then(() => this.clearDatEntries(datkey)).then(() => this.addDatToDb(datkey, dw.name, dw.directory)).finally(() => {this.dats[datkey] = dw;}).catch(e => console.log(e));}addDatToDb(dat, name, dir) {return this.db.insert({ dat, name, dir }).into('dats');}removeDatFromDb(datKey) {return this.db('dats').where('dat', datKey).del();} // Remove all entries for a dat
+  clearDatEntries(datKey) {return this.db('texts').where('dat', datKey).del();} // Adds an entry from a Dat
+  importDatEntry(dat, entry) {const arr = entry.name.split(_path2.default.sep);if (arr[0] === '') {arr.shift();}if (arr.length > 2) {console.log(_chalk2.default.bold('adding:'), entry.name);const name = (0, _anotherNameParser2.default)(arr[0]);return this.db.insert({ dat: dat.key, title_hash: '', file_hash: '', author: arr[0], author_sort: `${name.last}, ${name.first}`, title: arr[1], file: arr[2] }).into('texts');}return _bluebird2.default.resolve(false);} // Returns the path to a dat
+  // This is broken until i can understand making sqlite async
+  pathToDat(datKey) {return this.db.select('dir').from('dats').where('dat', datKey).first();} // Checks out an item by author and title and dat
+  checkout(author, title = false) {if (title) {return this.getDatsWithTitle(author, title).then(rows => this.download(author, title, rows.shift().dat));} // When there is no title specified, get every title
+    return this.getTitlesForAuthor(author).map(row => row).each(row => this.download(author, row.title, row.dat));} // Checks out an item by author and title and dat
+  download(author, title, datKey) {console.log(`checking out ${author}/${title} from ${datKey}`);return this.dats[datKey].downloadContent(_path2.default.join(author, title));} // Gets a count of authors in the catalog
+  search(query) {const s = `%${query}%`;return this.db('texts').where('title', 'like', s).orWhere('author', 'like', s).orderBy('author_sort', 'title_sort');} // Gets a count of authors in the catalog
+  getAuthors(startingWith = false) {const exp = this.db.select('author').from('texts').countDistinct('title as count');if (startingWith) {const s = `${startingWith}%`;exp.where('author_sort', 'like', s);}return exp.groupBy('author').orderBy('author_sort');} // Gets a list of letters of authors, for generating a directory
+  getAuthorLetters() {return this.db.column(this.db.raw('lower(substr(author_sort,1,1)) as letter')).select().from('texts').distinct('letter').orderBy('letter');}getTitlesForAuthor(author) {return this.db('texts').distinct('dat', 'title').where('author', author).orderBy('title');}getDatsWithTitle(author, title) {return this.db('texts').distinct('dat').where('author', author).where('title', title);} // Optionally only include files from a particular dat.
+  // Optionally specify a filename to find.
+  getFiles(author, title, dat = false, file = false) {const exp = this.db('texts').where('author', author).where('title', title);if (dat) {exp.where('dat', dat);}if (file) {exp.where('file', file);}return exp.orderBy('dat', 'file');} // Returns opf metadata object for an item, optionally preferring a specific library.
+  getOpf(author, title, dat = false) {const mfn = 'metadata.opf'; // metadata file name
+    return this.getFiles(author, title, dat, mfn).first().then(row => this.pathToDat(row.dat)).then(fp => (0, _opf.opf2js)(_path2.default.join(fp.dir, author, title, mfn)));}}exports.Catalog = Catalog;function createCatalog(dataDir = false) {// Directory to store all the data in
+  let dataDirFinal = _path2.default.join(process.cwd(), _config2.default.get('dataDir'));dataDirFinal = dataDir || dataDirFinal; // Create data directory if it doesn't exist yet
+  if (!_fs2.default.existsSync(dataDirFinal)) {_fs2.default.mkdirSync(dataDirFinal);}const catalog = new Catalog(dataDirFinal);return catalog.initDatabase().then(() => catalog);}exports.default = Catalog;
---
+++ dist/cli.js
@@ -0,0 +1,130 @@
+'use strict';var _path = require('path');var _path2 = _interopRequireDefault(_path);
+var _fs = require('fs');var _fs2 = _interopRequireDefault(_fs);
+var _catalog = require('./catalog');
+var _opf = require('./opf');
+var _config = require('./config');var _config2 = _interopRequireDefault(_config);function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+
+// Directory to store all the data in (should be a config option)
+const dataDir = _path2.default.join(process.cwd(), _config2.default.get('dataDir'));
+// Create data directory if it doesn't exist yet
+if (!_fs2.default.existsSync(dataDir)) {
+  _fs2.default.mkdirSync(dataDir);
+}
+
+let pending;
+
+// npm run cli --discover
+if (process.env.npm_config_discover) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.discoverDats()).
+  then(c => c.getAuthors()).
+  then(rows => {
+    console.log(`${rows.length} authors`);
+  });
+  // npm run cli --numAuthors
+} else if (process.env.npm_config_numAuthors) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.getAuthors()).
+  then(rows => {
+    console.log(`${rows.length} authors`);
+  });
+  // npm run cli --dat=datkey --name="A Nice Name"
+} else if (process.env.npm_config_dat) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  tap(c => c.discoverDats()).
+  then(c => c.importDat({
+    key: process.env.npm_config_dat,
+    name: process.env.npm_config_name,
+    sparse: true }));
+  // npm run cli --dir="/Path/to/Dir/to/Add" --name="A Nice Name"
+} else if (process.env.npm_config_dir) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  tap(c => c.discoverDats()).
+  then(c => c.importDir(process.env.npm_config_dir, process.env.npm_config_name)).
+  catch(e => console.log(e));
+  // npm run cli --checlout --author="A Name" --title="A Title"
+} else if (process.env.npm_config_checkout &&
+process.env.npm_config_author &&
+process.env.npm_config_title) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c =>
+  c.discoverDats().
+  then(() => c.getDatsWithTitle(process.env.npm_config_author, process.env.npm_config_title)).
+  then(rows => c.checkout(process.env.npm_config_author, process.env.npm_config_title, rows.shift().dat))).
+
+  finally(() => console.log('Finished downloading...'));
+  // npm run cli --listAuthors
+} else if (process.env.npm_config_listAuthors) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.getAuthors()).
+  then(rows => {
+    for (const doc of rows) {
+      console.log(`${doc.author} (${doc.count} items)`);
+    }
+  });
+  // npm run cli --search=query
+} else if (process.env.npm_config_search) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.search(process.env.npm_config_search)).
+  then(rows => {
+    for (const doc of rows) {
+      console.log(`${doc.author}, "${doc.title}"`);
+    }
+  });
+  // npm run cli --opf=/dir/to/file.opf
+} else if (process.env.npm_config_opf) {
+  pending = (0, _opf.opf2js)(process.env.npm_config_opf).
+  then(data =>
+  console.log(data.authors));
+
+  // npm run cli --getTitles --author="Author Name"
+} else if (process.env.npm_config_getTitles &&
+process.env.npm_config_author) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.getTitlesForAuthor(process.env.npm_config_author)).
+  then(rows => {
+    for (const doc of rows) {
+      console.log(`${doc.title} in dat:${doc.dat}`);
+    }
+  });
+  // npm run cli --getFiles --author="Author Name" --title="Title of text"
+} else if (process.env.npm_config_getFiles &&
+process.env.npm_config_author &&
+process.env.npm_config_title) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.getFiles(process.env.npm_config_author, process.env.npm_config_title)).
+  then(rows => {
+    for (const doc of rows) {
+      console.log(`${doc.file} in dat:${doc.dat}`);
+    }
+  });
+  // npm run cli --getOpf --author="Author Name" --title="Title of text"
+} else if (process.env.npm_config_getOpf &&
+process.env.npm_config_author &&
+process.env.npm_config_title) {
+  pending = (0, _catalog.createCatalog)(dataDir).
+  then(c => c.getOpf(process.env.npm_config_author, process.env.npm_config_title)).
+  then(opf => {
+    console.log(`Title: ${opf.title}`);
+    console.log(`Authors: ${opf.authors}`);
+    console.log('Identifiers:');
+    for (const i of opf.identifiers) {
+      console.log(i);
+    }
+    console.log(`Description: ${opf.description}`);
+    console.log(`An undefined field: ${opf.undefined}`);
+  });
+} else {// by default print help
+  console.log('--dat=datkey --name="A Nice Name"\tImport a new dat to your catalog');
+  console.log('--dir="/Path/to/Dir/to/Add" --name="A Nice Name"\tImport a local directory');
+  console.log('--checkout --author="A Name" --title="A Title"');
+  console.log('--numAuthors\tPrint the number of authors in the catalog');
+  console.log('--listAuthors\tLists the authors in the catalog');
+  console.log('--search\tQueries the catalog');
+  console.log('--getTitles --author="Author Name"');
+  console.log('--getFiles --author="Author Name" --title="Title of text"');
+  console.log('--getOpf --author="Author Name" --title="Title of text"');
+  console.log('--opf=/dir/to/file.opf');
+}
+
+Promise.resolve(pending).catch(console.error);
