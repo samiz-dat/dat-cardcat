a92d1dd7b032ede0557d64dd95a5407119096418
e-e-e <e-e-e@users.noreply.github.com>
2017-05-22 22:06:48 +1000

    begin setting up infrastructure for testing.

---
+++ test/catalog.test.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('fork a library', () => {
+  it('needs a real test', () => {
+    expect('us to write some real tests soon').to.have.string('tests');
+  });
+});
