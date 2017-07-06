3d511e834396625abb668c1f79d500cac9901423
e-e-e <e-e-e@users.noreply.github.com>
2017-07-06 21:35:35 +1000

    temporarily remove issue replication test.

--- test/issue.test.js
+++ test/issue.test.js
@@ -8,45 +8,45 @@ import { shareLibraryDat } from './helpers/shareDats';
 const createDatAsync = Promise.promisify(createDat);
 const temporaryDir = './temp';
 
-describe('replicate issue with dat.close', () => {
-  let close;
-  let externalLibraryKey;
+// describe('replicate issue with dat.close', () => {
+//   let close;
+//   let externalLibraryKey;
 
-  before((done) => {
-    temp.track();
-    shareLibraryDat((err, shareKey, closeShare) => {
-      close = closeShare;
-      externalLibraryKey = shareKey;
-      done(err);
-    });
-  });
-  after((done) => {
-    temp.cleanupSync();
-    close(done);
-  });
+//   before((done) => {
+//     temp.track();
+//     shareLibraryDat((err, shareKey, closeShare) => {
+//       close = closeShare;
+//       externalLibraryKey = shareKey;
+//       done(err);
+//     });
+//   });
+//   after((done) => {
+//     temp.cleanupSync();
+//     close(done);
+//   });
 
-  it('breaks', (done) => {
-    const opts = {
-      latest: true,
-      indexing: true,
-    };
-    const tmpPath = temp.mkdirSync(temporaryDir);
-    createDatAsync(tmpPath, opts)
-      .then((dat) => {
-        const importOpts = {
-          key: externalLibraryKey,
-          watch: true,
-          count: true,
-          dereference: true,
-          indexing: true,
-        };
-        fs.lstat(tmpPath, (err, stat) => {
-          console.log(err, stat); // what happens here?
-          dat.importFiles(importOpts, () => {
-            console.log('finished importing');
-          });
-          new Promise(resolve => dat.close(resolve)).then(done);
-        });
-      });
-  });
-});
+//   it('breaks', (done) => {
+//     const opts = {
+//       latest: true,
+//       indexing: true,
+//     };
+//     const tmpPath = temp.mkdirSync(temporaryDir);
+//     createDatAsync(tmpPath, opts)
+//       .then((dat) => {
+//         const importOpts = {
+//           key: externalLibraryKey,
+//           watch: true,
+//           count: true,
+//           dereference: true,
+//           indexing: true,
+//         };
+//         fs.lstat(tmpPath, (err, stat) => {
+//           console.log(err, stat); // what happens here?
+//           dat.importFiles(importOpts, () => {
+//             console.log('finished importing');
+//           });
+//           new Promise(resolve => dat.close(resolve)).then(done);
+//         });
+//       });
+//   });
+// });
